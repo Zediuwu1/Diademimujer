1949ecf1eb7304ecc196c71523aae83b6f30191e
Zediuwu <jeremy55468@gmail.com>
2025-03-07 02:22:37 -0500

    Changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    server: {
+      hmr: false // Deshabilita el Hot Module Reloading (HMR), que incluye el toolbar
+    }
+  }
+});
